98890c14a3d4ac8869b34a2db0a124d9ca7df6a7
beccakunce <rebeccakunce@gmail.com>
2026-09-17 01:05:27 -0500

    adding to the love bomb controller and still connected to postman

--- src/main/java/com/launchcode2026/kunce_self_love_bombs/controllers/LoveBombController.java
+++ src/main/java/com/launchcode2026/kunce_self_love_bombs/controllers/LoveBombController.java
@@ -1,9 +1,31 @@
 package com.launchcode2026.kunce_self_love_bombs.controllers;
 
+import com.launchcode2026.kunce_self_love_bombs.models.LoveBomb;
+import com.launchcode2026.kunce_self_love_bombs.repositories.CheckInRepository;
+import com.launchcode2026.kunce_self_love_bombs.repositories.LoveBombRepository;
+import com.launchcode2026.kunce_self_love_bombs.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.GetExchange;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("love-bomb")
 public class LoveBombController {
+
+    @Autowired
+    private LoveBombRepository loveBombRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private CheckInRepository checkInRepository;
+
+    @GetExchange("find-all")
+    public List<LoveBomb> getAll() {
+        return loveBombRepository.findAll();
+    }
 }
