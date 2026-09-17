d048ef238b2dce6ae6b6dee404d1f583b42c415d
beccakunce <rebeccakunce@gmail.com>
2026-09-16 20:05:26 -0500

    initial commit

---
+++ src/main/java/com/launchcode2026/kunce_self_love_bombs/KunceSelfLoveBombsApplication.java
@@ -0,0 +1,13 @@
+package com.launchcode2026.kunce_self_love_bombs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KunceSelfLoveBombsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KunceSelfLoveBombsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/launchcode2026/kunce_self_love_bombs/KunceSelfLoveBombsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.launchcode2026.kunce_self_love_bombs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KunceSelfLoveBombsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
