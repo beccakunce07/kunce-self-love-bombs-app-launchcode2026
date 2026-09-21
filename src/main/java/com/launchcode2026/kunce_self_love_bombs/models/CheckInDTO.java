23006bf1108b6a6ff1336117a5d45a1a08d68631
beccakunce <rebeccakunce@gmail.com>
2026-09-21 12:09:17 -0500

    changed localdate to local date time

--- src/main/java/com/launchcode2026/kunce_self_love_bombs/models/CheckInDTO.java
+++ src/main/java/com/launchcode2026/kunce_self_love_bombs/models/CheckInDTO.java
@@ -3,14 +3,14 @@ package com.launchcode2026.kunce_self_love_bombs.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class CheckInDTO {
 
     @Id
     private String key;
     private String feeling;
-    private LocalDate recordedAt;
+    private LocalDateTime recordedAt;
 
     public String getKey() {
         return key;
@@ -28,11 +28,11 @@ public class CheckInDTO {
         this.feeling = feeling;
     }
 
-    public LocalDate getRecordedAt() {
+    public LocalDateTime getRecordedAt() {
         return recordedAt;
     }
 
-    public void setRecordedAt(LocalDate recordedAt) {
+    public void setRecordedAt(LocalDateTime recordedAt) {
         this.recordedAt = recordedAt;
     }
 }
