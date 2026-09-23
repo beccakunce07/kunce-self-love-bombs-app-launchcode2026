cbdf2fe61711c4be9933fe2a2e3c8bab8be83c29
beccakunce <rebeccakunce@gmail.com>
2026-09-23 01:30:16 -0500

    editing check in page to fetch api

--- FRONTENDAPP/src/pages/CheckInPage.jsx
+++ FRONTENDAPP/src/pages/CheckInPage.jsx
@@ -5,15 +5,65 @@ import PageHeader from '../components/PageHeader';
 
 function CheckInPage () {
   
-  const [key, setKey] = useState("");
+  const [errors, setErrors] = useState({});
+  const [categoryKey, setCategoryKey] = useState("");
   const [feeling, setFeeling] = useState("");
+  const [checkInList, setCheckInList] = useState ([]);
+
+  const validate = () => {
+    let newErrors = {};
+    if (!feeling) newErrors.feeling = "oops. please choose a feeling";
+    if (!categoryKey) newErrors.categoryKey = "whoopsie poopsie. please choose a category"
+
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0
+  };
+
+  const handleCheckInSubmit = async (e) => {
+    e.preventDefault();
+
+  if(validate()){
+    const checkInData = {
+      categoryKey : categoryKey,
+      feeling : feeling,
+      recordedAt : new Date().toISOString()
+    };
+
+      try {
+        const response = await fetch ('http://localhost:8080/check-in/create-check-in', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(checkInData)
+            });
+
+            if (response.ok) {
+              const savedCheckIn = await response.json();
+              setCheckInList([...checkInList, savedCheckIn]);
+              console.log("Check-in added successfully. Thank you.", savedCheckIn);
+            }
+
+            //clearing the use state
+            setFeeling("");
+            setCategoryKey("");
+
+            setErrors({});
+          } catch (error){
+            console.error('Oh dear. There was an error saving your check-in', error)
+          }
+    }
+  }
+
 
   return (
     <>
     <PageHeader title = "Let's Check In"/>
-    <div className="page-wrapper">
-      <div className='content-card'>
+    {/* <div className="page-wrapper"> */}
+      <form className='content-card' onSubmit = {handleCheckInSubmit}>
       <h2> Today I am feeling...</h2>
+
+      {errors.feeling && <p className="error">{errors.feeling}</p>}
+      {errors.categoryKey && <p className="error">{errors.categoryKey}</p>}
+
       <div className = "button-container">
       <button className = "button1" onClick={() => setFeeling("sad")}>Sad</button>
       <button className = "button1" onClick={() => setFeeling("angry")}>Angry</button>
@@ -28,19 +78,23 @@ function CheckInPage () {
       <h3> About my...</h3>
       
       <div className = "button-container">
-      <button className = "button2" onClick={() => setKey("finances")}>Finances</button>
-      <button className = "button2" onClick={() => setKey("body")}>Body</button>
-      <button className = "button2" onClick={() => setKey("relationship")}>Relationship</button>
-      <button className = "button2" onClick={() => setKey("purpose")}>Purpose</button>
-      <button className = "button2" onClick={() => setKey("life in general")}>Life in general</button>
-      <button className = "button2" onClick={() => setKey("something else")}>Something Else</button>
+      <button className = "button2" onClick={() => setCategoryKey("finances")}>Finances</button>
+      <button className = "button2" onClick={() => setCategoryKey("body")}>Body</button>
+      <button className = "button2" onClick={() => setCategoryKey("relationship")}>Relationship</button>
+      <button className = "button2" onClick={() => setCategoryKey("purpose")}>Purpose</button>
+      <button className = "button2" onClick={() => setCategoryKey("life in general")}>Life in general</button>
+      <button className = "button2" onClick={() => setCategoryKey("something else")}>Something Else</button>
       </div>
       
-      <p>About my {key}.</p>   
+      <p>About my {categoryKey}.</p>   
       
-      <SpecificSlbButton category = {key}/>
-    </div>
-    </div>
+      <SpecificSlbButton category = {categoryKey}/>
+      <button type = "submit" className = "button">Log Check-In</button>
+
+    
+      </form>
+      {/* </div> */}
+  
 </>
   
   )
